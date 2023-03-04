395c3611cd9ff9b9c110c06fc67b3df3cd942b6e
uqarni <108438834+uqarni@users.noreply.github.com>
2023-03-04 07:16:10 +0000

    tried to ifx latin-1 issue but failed. terminal script works in debut mode

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
-import openai
-import os 
 import streamlit as st
+import os
+import openai
 
-apikey = os.environ['OPENAI_API_KEY']
-openai.api_key = apikey
+OPENAI_API_KEY = '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'
+openai.api_key = OPENAI_API_KEY
+
+# st.title("Document Bot")
+# uploaded_file = st.file_uploader("Choose a file")
+
+# if uploaded_file is not None:
+#     with open("text.txt","wb") as f:
+#         f.write(uploaded_file.getbuffer())
+#     st.success("File uploaded")
+
+# user_question = st.text_input(
+#     "Enter Your Question : ",
+#     placeholder = "Cyanobacteria can perform photosynthetsis , are they considered as plants?",
+# )
+
+# if st.button("Tell me about it", type = "primary"):
+loader = TextLoader('text.txt')
+index = VectorstoreIndexCreator().from_loaders([loader])
+query = "What does FAIR stand for?"
+answer = index.query_with_sources(query)
+print(answer)
+#     st.success(answer['answer'])
 
 
-st.title("Document Bot")
+# os.system('streamlit run main.py --server.enableCORS false --server.enableXsrfProtection false')
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # Open a file to write the contents of the uploaded file
-    with open("text.txt", "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    st.success("File saved as text.txt!")
 
-user_question = st.text_input(
-    "Enter Your Question : ",
-    placeholder = "Cyanobacteria can perform photosynthetsis , are they considered as plants?",
-)
-
-if os.path.exists("text.txt"):
-    loader = TextLoader('text.txt')
-    index = VectorstoreIndexCreator().from_loaders([loader])
-
-if st.button("Tell me about it", type = "primary"):
-    query = user_question
-    answer = index.query_with_sources(query)
-    st.success(answer['answer'])
 
